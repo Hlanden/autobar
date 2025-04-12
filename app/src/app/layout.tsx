47fdc852be387b04f09ce2db113154b5d1e425db
Jørgen Haaland <jorgen.haaland@wheel.me>
2025-04-12 17:20:59 +0200

    Add material ui example page for reference

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import theme from "@/theme";
-import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
